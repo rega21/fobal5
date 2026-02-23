8e51d50709ecce0531aac7cf209042e6f8f1155b
rega21 <aregaarrospide@gmail.com>
2026-02-23 13:02:04 -0300

    chore: update public Google Maps API key

--- config.public.js
+++ config.public.js
@@ -2,5 +2,5 @@ window.APP_CONFIG = {
   AUTH_LOGIN_URL: "https://zguqyimgxppglgrblvim.supabase.co/functions/v1/admin-login",
   SUPABASE_ANON_KEY:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpndXF5aW1neHBwZ2xncmJsdmltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3MzY0NzAsImV4cCI6MjA4NjMxMjQ3MH0.DKc0u44Ja2Z3y01MBXS_YwkOG_DK4mrRnAUrT2fJniU",
-  GOOGLE_MAPS_API_KEY: "AIzaSyDOCrsdyJMptBf7bGcXIcXmjKzrKX-zS1s",
+  GOOGLE_MAPS_API_KEY: "AIzaSyBJnkaB0t85cVHVF0SqHfKXAu7mY9p9QaA",
 };
